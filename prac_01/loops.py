d2d08631ad75ac4892f27343aa07bf9725dddb21
wade-sheedy <30946766+wade-sheedy@users.noreply.github.com>
2018-02-27 10:36:18 +1000

    Loops

---
+++ prac_01/loops.py
@@ -0,0 +1,12 @@
+for i in range(1, 21, 2):
+    print(i, end=' ')
+    print()
+
+for i in range(0, 101, 10):
+    print(i, end=' ')
+    print()
+
+for i in range(21, 0, -1):
+    print(i, end=' ')
+    print()
+    
