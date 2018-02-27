ac61f0bc330862d33356825d531049dc1e09e9bb
wade-sheedy <30946766+wade-sheedy@users.noreply.github.com>
2018-02-27 10:51:07 +1000

    sales_bonus_2.0

---
+++ prac_01/sales_bonus_2.0.py
@@ -0,0 +1,14 @@
+"""Program to calculate and display a user's bonus based on sales.
+If sales are under $1,000, the user gets a 10% bonus.
+If Sales are $1000 or over the bonus is 15%"""
+
+sales = float(input("Enter Sales:$ "))
+###print(sales)- test statement for sales input
+while sales > 0:
+    if sales < 1000:
+        bonus = sales * 0.1
+        print(bonus)
+    elif sales >= 1000:
+        bonus = sales * 0.15
+        print(bonus)
+    sales = float(input("Enter Sales:$ "))
