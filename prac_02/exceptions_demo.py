1169799eeabd713c945f60e66c1e94ba60203dab
wade-sheedy <30946766+wade-sheedy@users.noreply.github.com>
2018-03-07 19:00:26 +1000

    Add files via upload

--- prac_02/exceptions_demo.py
+++ prac_02/exceptions_demo.py
@@ -3,25 +3,25 @@ CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur? i got a value error when i input the number in the format of a string '5'
 2. When will a ZeroDivisionError occur? When a input of 0 was placed in for the denominator a ZeroDivisionError
-3. Could you change the code to avoid the possibility of a ZeroDivisionError? using the logic of limits so that if x = 0 we can make x infinatly smaller
-        #  so it gets close to 0 but it never reaches 0
-        # 10^-15 is very close to zero but it is not yet zero
+3. instead of useing a except if i do an if statement before the program  gets to the division and some input validation occurs i can mae
 """
-import math
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    if denominator == 0:
-        denominator = 'Infinity'
-        print("The fraction has reached a limit and the denominator has reached a limit and has achieved {} ".format(denominator))
-    else:
-        fraction = numerator / denominator
-        print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-#except ZeroDivisionError:
-    #denominator = 1 * math.pow(10, 15)
-    #fraction = numerator / denominator
-    #print(fraction)
-    #print("Cannot divide by zero! converting to a number close to zero but not zero")
-print("Finished.")
+while True:
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+        if denominator == 0:
+            denominator = 'Infinity'
+            print("Cannot Divide by zero enter a valid denominator!!!")
+        else:
+            fraction = numerator / denominator
+            print(fraction)
+            print("Finished.")
+            break
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+    #except ZeroDivisionError:
+        #denominator = 1 * math.pow(10, 15)
+        #fraction = numerator / denominator
+        #print(fraction)
+        #print("Cannot divide by zero! converting to a number close to zero but not zero")
+
